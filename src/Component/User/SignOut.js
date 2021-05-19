da8edb0328cbf37f861876d31f0302b5a0a32828
AliAsadi <4.aliasadi.4@gmai.com>
2021-05-19 14:49:57 +0430

    fix signout

--- src/Component/User/SignOut.js
+++ src/Component/User/SignOut.js
@@ -2,7 +2,6 @@ import serverURL from "../../RequestConfig/serverURL";
 import TokenConfig from "../../RequestConfig/TokenConfig";
 import axios from 'axios' ;
 export const signOut = (props) =>{ 
-  alert(props)
       axios.post( serverURL() + props +"/logout/" ,{},TokenConfig() )
           .then(res =>{
               console.log(res);

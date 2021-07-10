3e2e916a780d15db3f33a0f3eaa665dec9e2fd40
mojtaba <mojtabanafez96@gmail.com>
2021-07-10 21:12:16 +0430

    about us

--- src/Component/LandingPage/Home.js
+++ src/Component/LandingPage/Home.js
@@ -74,28 +74,25 @@ class Home extends Component {
                         <section class="text-center py-5 grey lighten-4" id="about">
                             <div class="container">
                                 <div class="wow fadeIn">
-                                    <h2 class="h1 pt-5 pb-3">چرا پرگار؟</h2>
+                                    <h2 class="h1 pt-5 pb-3">چرا پرگار را انتخاب کنیم؟</h2>
                                 </div>
                                 <div class="row">
                                     <div class="col-md-4 mb-5 wow fadeInUp" data-wow-delay=".3s"><i class="fas fa-tachometer-alt fa-3x orange-text"></i>
                                         <h3 class="h4 mt-3">سرعت پاسخگویی</h3>
                                         <p class="mt-3 blue-grey-text">
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima assumenda deleniti
-                                            hic.
+                                           
                                         </p>
                                     </div>
                                     <div class="col-md-4 mb-5 wow fadeInUp" data-wow-delay=".4s"><i class="fas fa-address-card fa-3x cyan-text"></i>
                                         <h3 class="h4 mt-3">مشاوران با تجربه</h3>
                                         <p class="mt-3 blue-grey-text">
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima assumenda deleniti
-                                            hic.
+                                          
                                         </p>
                                     </div>
                                     <div class="col-md-4 mb-5 wow fadeInUp" data-wow-delay=".5s"><i class="fas fa-shield-alt fa-3x"></i>
                                         <h3 class="h4 mt-3">حفط حریم شخصی</h3>
                                         <p class="mt-3 blue-grey-text">
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima assumenda deleniti
-                                            hic.
+                                            
                                         </p>
                                     </div>
                                 </div>
@@ -155,28 +152,27 @@ class Home extends Component {
                             <div class="rgba-black-strong py-5">
                                 <div class="container">
                                     <div class="wow fadeIn">
-                                        <h2 class="h1 text-white pt-5 pb-3 text-center">Contact us</h2>
+                                        <h2 class="h1 text-white pt-5 pb-3 text-center">تماس با ما</h2>
                                         <p class="text-white px-5 mb-5 pb-3 lead text-center">
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error amet numquam iure provident voluptate
-                                            esse quasi, veritatis totam voluptas nostrum quisquam eum porro a pariatur accusamus veniam.
+                                           
                                         </p>
                                     </div>
                                     <div class="card mb-5 wow fadeInUp" data-wow-delay=".4s">
                                         <div class="card-body p-5">
                                             <div class="row">
                                                 <div class="col-md-8">
-                                                    <form action="https://formspree.io/youremail@example.com" method="POST">
+                                                    <form action="https://formspree.io/consultant.iust.se@gmail.com" method="POST">
                                                         <div class="row">
                                                             <div class="col-md-6">
                                                                 <div class="md-form">
                                                                     <input class="form-control" id="name" type="text" name="name" required="required" />
-                                                                    <label for="name">Your name</label>
+                                                                    <label for="name">نام کامل</label>
                                                                 </div>
                                                             </div>
                                                             <div class="col-md-6">
                                                                 <div class="md-form">
                                                                     <input class="form-control" id="email" type="text" name="_replyto" required="required" />
-                                                                    <label for="email">Your email</label>
+                                                                    <label for="email">ایمیل شما</label>
                                                                 </div>
                                                             </div>
                                                         </div>
@@ -184,31 +180,31 @@ class Home extends Component {
                                                             <div class="col-md-12">
                                                                 <div class="md-form">
                                                                     <input class="form-control" id="subject" type="text" name="subject" required="required" />
-                                                                    <label for="subject">Subject</label>
+                                                                    <label for="subject">موضوع</label>
                                                                 </div>
                                                             </div>
                                                             <div class="col-md-12">
                                                                 <div class="md-form">
                                                                     <textarea class="md-textarea" id="message" name="message" required="required"></textarea>
-                                                                    <label for="message">Your message</label>
+                                                                    <label for="message">پیام شما</label>
                                                                 </div>
                                                             </div>
                                                         </div>
                                                         <div class="center-on-small-only mb-4">
-                                                            <button class="btn btn-indigo ml-0" type="submit"><i class="fa fa-paper-plane-o mr-2"></i> Send</button>
+                                                            <button class="btn btn-indigo ml-0" type="submit"><i class="fa fa-paper-plane-o mr-2"></i> ارسال</button>
                                                         </div>
                                                     </form>
                                                 </div>
                                                 <div class="col-md-4">
                                                     <ul class="list-unstyled text-center">
                                                         <li class="mt-4"><i class="fa fa-map-marker indigo-text fa-2x"></i>
-                                                            <p class="mt-2">140, City Center, New York, U.S.A</p>
+                                                            <p class="mt-2">۱۴۰, نارمک, تهران, ایران</p>
                                                         </li>
                                                         <li class="mt-4"><i class="fa fa-phone indigo-text fa-2x"></i>
-                                                            <p class="mt-2">+ 01 234 567 89</p>
+                                                            <p class="mt-2">+ ۹۸ ۹۱۸ ۷۴۹ ۶۰ ۹۷</p>
                                                         </li>
                                                         <li class="mt-4"><i class="fa fa-envelope indigo-text fa-2x"></i>
-                                                            <p class="mt-2">contact@company.com</p>
+                                                            <p class="mt-2">consultant.iust.se@gmail.com</p>
                                                         </li>
                                                     </ul>
                                                 </div>
